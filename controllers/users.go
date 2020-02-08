1f8133ee669f90579ad9e1dea50932d4025498f4
Andrew Arrow <andrew@bird.co>
2020-02-08 00:58:11 -0800

    users

--- controllers/users.go
+++ controllers/users.go
@@ -4,15 +4,17 @@ import "github.com/gin-gonic/gin"
 import "net/http"
 
 func UsersIndex(c *gin.Context) {
-	c.HTML(http.StatusOK, "index.tmpl", gin.H{
+	c.HTML(http.StatusOK, "users__index.tmpl", gin.H{
 		"flash": "",
+		"users": []string{},
 		"name":  "name",
 	})
 
 }
 func UsersShow(c *gin.Context) {
-	c.HTML(http.StatusOK, "index.tmpl", gin.H{
+	c.HTML(http.StatusOK, "users__show.tmpl", gin.H{
 		"flash": "",
+		"user":  map[string]string{},
 		"name":  "name",
 	})
 
--- server/templates.go
+++ server/templates.go
@@ -10,7 +10,7 @@ import "github.com/justincampbell/timeago"
 import "html/template"
 
 func AddTemplates(r *gin.Engine, prefix string) {
-	r.SetFuncMap(template.FuncMap{
+	fm := template.FuncMap{
 		"mod": func(i, j int) bool { return i%j == 0 },
 		"ago": func(i int64) string {
 			d, _ := time.ParseDuration(fmt.Sprintf("%ds", time.Now().Unix()-i))
@@ -22,6 +22,7 @@ func AddTemplates(r *gin.Engine, prefix string) {
 			md := blackfriday.Run([]byte(s))
 			return template.HTML(string(md))
 		},
-	})
+	}
+	r.SetFuncMap(fm)
 	r.LoadHTMLGlob(prefix + "templates/*.tmpl")
 }
