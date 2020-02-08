e4dd29cd9ad964e95c6b252edce97abc4f39f602
Andrew Arrow <andrew@bird.co>
2020-02-08 01:24:29 -0800

    is user nil

--- controllers/welcome.go
+++ controllers/welcome.go
@@ -4,10 +4,13 @@ import "github.com/gin-gonic/gin"
 import "net/http"
 import "github.com/jmoiron/sqlx"
 
+//import "github.com/andrewarrow/feedback/models"
+
 var Db *sqlx.DB
 
 func WelcomeIndex(c *gin.Context) {
 	c.HTML(http.StatusOK, "index.tmpl", gin.H{
+		"user":  nil,
 		"flash": "",
 		"name":  "name",
 	})
