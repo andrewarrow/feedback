4fcf83f98a760e73c04f2d27736cf624ea7d84cd
Andrew Arrow <andrew@bird.co>
2020-02-08 18:04:19 -0800

    logic

--- controllers/welcome.go
+++ controllers/welcome.go
@@ -3,19 +3,20 @@ package controllers
 import "github.com/gin-gonic/gin"
 import "net/http"
 import "github.com/jmoiron/sqlx"
-import "fmt"
-
+import "github.com/andrewarrow/feedback/util"
 import "github.com/andrewarrow/feedback/models"
 
 var Db *sqlx.DB
 
 func WelcomeIndex(c *gin.Context) {
+	flash, _ := c.Cookie("flash")
+	host := util.AllConfig.Http.Host
+	c.SetCookie("flash", "", 3600, "/", host, false, false)
 	json, _ := c.Cookie("user")
-	fmt.Println("11111111111111", json)
 	user := models.DecodeUser(json)
 	c.HTML(http.StatusOK, "index.tmpl", gin.H{
 		"user":  user,
-		"flash": "",
+		"flash": flash,
 		"name":  "name",
 	})
 
