9e7cf27940688de7da4c376387b63dac16acefe5
Andrew Arrow <andrew@bird.co>
2020-03-04 00:16:11 -0800

    terms

--- controllers/legal.go
+++ controllers/legal.go
@@ -15,14 +15,21 @@ func LegalGdpr(c *gin.Context) {
 }
 
 func LegalPrivacy(c *gin.Context) {
+	host := util.AllConfig.Http.Host
 	c.HTML(http.StatusOK, "privacy.tmpl", gin.H{
 		"flash": "",
+		"name":  "feedback", // hint: change me
+		"host":  host,
 	})
 
 }
 func LegalTerms(c *gin.Context) {
+	host := util.AllConfig.Http.Host
+
 	c.HTML(http.StatusOK, "terms.tmpl", gin.H{
 		"flash": "",
+		"name":  "feedback", // hint: change me
+		"host":  host,
 	})
 
 }
