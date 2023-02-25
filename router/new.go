4ddde3681908d560450c0b9dbdb94a0a1b90e3d7
Andrew Arrow <oneone@gmail.com>
2023-02-25 07:19:03 -0800

    admin users start

--- router/new.go
+++ router/new.go
@@ -1,9 +1,12 @@
 package router
 
 type Router struct {
+	Paths map[string]string
 }
 
 func NewRouter() *Router {
 	r := Router{}
+	r.Paths = map[string]string{}
+	r.Paths["/admin/users"] = "GET"
 	return &r
 }
--- router/path.go
+++ router/path.go
@@ -9,14 +9,21 @@ import (
 
 func (r *Router) RouteFromRequest(writer http.ResponseWriter, request *http.Request) {
 	path := request.URL.Path
+	fmt.Println(path)
 	if path == "/" {
 		welcome := files.ReadFile("views/welcome.html")
 		fmt.Fprintf(writer, welcome)
 	} else if path == "/feedback/add" {
 		fmt.Fprintf(writer, "ok")
 	} else {
-		writer.WriteHeader(404)
-		notFound := files.ReadFile("views/404.html")
-		fmt.Fprintf(writer, notFound)
+		match := r.Paths[path]
+		if match == "" {
+			writer.WriteHeader(404)
+			notFound := files.ReadFile("views/404.html")
+			fmt.Fprintf(writer, notFound)
+		} else {
+			matchFile := files.ReadFile(fmt.Sprintf("views%s.html", path))
+			fmt.Fprintf(writer, matchFile)
+		}
 	}
 }
