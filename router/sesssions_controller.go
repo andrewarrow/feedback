00efc9a26f6574be8065f01394ab5a1e3daf16a0
Andrew Arrow <oneone@gmail.com>
2023-03-04 08:01:14 -0800

    removing router/controller.go file

--- router/sesssions_controller.go
+++ router/sesssions_controller.go
@@ -48,27 +48,3 @@ func CreateSession(c *Context) {
 	http.SetCookie(c.writer, &cookie)
 	http.Redirect(c.writer, c.request, "/", 302)
 }
-
-/*
-type SessionsController struct {
-}
-
-func NewSessionsController() Controller {
-	sc := SessionsController{}
-	return &sc
-}
-
-func (sc *SessionsController) New(c *Context) {
-}
-
-func (sc *SessionsController) Index(context *Context) {
-}
-
-func (sc *SessionsController) Show(c *Context, id string) {
-}
-
-func (sc *SessionsController) CreateWithId(c *Context, id string) {
-}
-
-func (sc *SessionsController) CreateWithJson(context *Context, body string) {
-}*/
