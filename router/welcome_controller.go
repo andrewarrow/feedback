3b00e16f9dcde4bbb32c292777e9ed4dac2e778d
Andrew Arrow <oneone@gmail.com>
2023-03-04 15:18:35 -0800

    using struct vs map

--- router/welcome_controller.go
+++ router/welcome_controller.go
@@ -2,39 +2,43 @@ package router
 
 import (
 	"fmt"
-	"strings"
 	"time"
 
 	"github.com/jmoiron/sqlx"
 	"github.com/xeonx/timeago"
 )
 
+type Story struct {
+	Url       string
+	Title     string
+	Guid      string
+	Ago       string
+	Timestamp string
+	Username  string
+}
+
 type WelcomeVars struct {
-	Rows []map[string]any
+	Rows []*Story
 }
 
 const layout = "2006-01-02 15:04:05"
 
 func WelcomeIndexVars(db *sqlx.DB, location *time.Location) *WelcomeVars {
 	vars := WelcomeVars{}
-	vars.Rows = []map[string]any{}
+	vars.Rows = []*Story{}
 	rows, _ := db.Queryx("SELECT * FROM stories ORDER BY created_at desc limit 30")
 	for rows.Next() {
 		m := make(map[string]any)
 		rows.MapScan(m)
-		for k, v := range m {
-			m[k] = fmt.Sprintf("%s", v)
-		}
-		tokens := strings.Split(m["created_at"].(string), ".")
-		timestamp := tokens[0]
-		tm, _ := time.Parse(layout, timestamp)
+		story := Story{}
+		story.Title = fmt.Sprintf("%s", m["title"])
+		story.Url = fmt.Sprintf("%s", m["url"])
+
+		tm := m["created_at"].(time.Time)
 		tm = tm.Add(time.Hour * 8)
-		m["timestamp"] = timestamp
-		m["ago"] = timeago.English.Format(tm)
-		if m["url"].(string) == "" {
-			m["url"] = "/stories/" + m["guid"].(string) + "/"
-		}
-		vars.Rows = append(vars.Rows, m)
+		story.Timestamp = fmt.Sprintf("%s", tm)
+		story.Ago = timeago.English.Format(tm)
+		vars.Rows = append(vars.Rows, &story)
 	}
 	return &vars
 }
