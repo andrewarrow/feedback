0e465e7e2a138070be0d11c7ce6373afdc3f75e6
Andrew Arrow <oneone@gmail.com>
2023-03-05 07:27:29 -0800

    fixing fmt.Fprintf(writer issue with writer.Write bytes

--- router/assets.go
+++ router/assets.go
@@ -12,10 +12,16 @@ func (r *Router) HandleAsset(path string, writer http.ResponseWriter) {
 	contentType := "text/css"
 	if strings.HasSuffix(path, ".js") {
 		contentType = "application/javascript"
+	} else if strings.HasSuffix(path, ".ico") {
+		contentType = "image/x-icon"
 	} else if strings.HasSuffix(path, ".gif") {
 		contentType = "image/gif"
 	}
 	writer.Header().Set("Content-Type", contentType)
+	writer.Header().Set("Connection", "keep-alive")
+	//writer.Header().Set("Accept-Ranges", "bytes")
+	//writer.Header().Set("Vary", "Accept-Encoding")
 	matchFile := files.ReadFile(fmt.Sprintf("%s", path[1:]))
-	fmt.Fprintf(writer, matchFile)
+	writer.Header().Set("Content-Length", fmt.Sprintf("%d", len(matchFile)))
+	writer.Write([]byte(matchFile))
 }
