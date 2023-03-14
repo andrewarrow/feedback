b1bced68367835970ac08bab05638cb90d96b49b
Andrew Arrow <oneone@gmail.com>
2023-03-14 14:08:05 -0700

    using template vs Sprintf

--- gogen/files.go
+++ gogen/files.go
@@ -1,7 +1,9 @@
 package gogen
 
 import (
+	"bytes"
 	"fmt"
+	"text/template"
 
 	"github.com/andrewarrow/feedback/files"
 	"github.com/andrewarrow/feedback/util"
@@ -11,23 +13,60 @@ func MakeControllerAndView(name, dir string) {
 	c := `package app
 
 import (
-  "github.com/andrewarrow/feedback/router"
+	"github.com/andrewarrow/feedback/router"
 )
 
-func Handle%s(c *router.Context, second, third string) {
-  if c.User == nil {
-    c.UserRequired = true
-    return
-  }
-  c.SendContentInLayout("%s_index.html", nil, 200)
+func Handle{{index . "camel" }}(c *router.Context, second, third string) {
+	if c.User == nil {
+		c.UserRequired = true
+		return
+	}
+	if second == "" {
+		handle{{index . "camel" }}Index(c)
+	} else if second != "" && third == "" {
+		handle{{index . "camel" }}Show(c, second)
+	} else {
+		c.NotFound = true
+	}
+}
+
+func handle{{index . "camel" }}Index(c *router.Context) {
+	if c.Method == "GET" {
+		c.SendContentInLayout("{{index . "name" }}_index.html", nil, 200)
+		return
+	}
+	handle{{index . "camel" }}Create(c)
+}
+
+func handle{{index . "camel" }}Create(c *router.Context) {
+	c.NotFound = true
+}
+
+func handle{{index . "camel" }}Show(c *router.Context, id string) {
+	if c.Method == "GET" {
+		c.SendContentInLayout("{{index . "name" }}_show.html", nil, 200)
+		return
+	}
+	handle{{index . "camel" }}Updates(c, id)
+}
+
+func handle{{index . "camel" }}Updates(c *router.Context, id string) {
+	if c.Method == "POST" {
+		c.NotFound = true
+	} else if c.Method == "DELETE" {
+		c.NotFound = true
+	}
 }`
 
-	camel := util.ToCamelCase(name)
-	controller := fmt.Sprintf(c, camel, name)
+	m := map[string]string{"name": name, "camel": util.ToCamelCase(name)}
+	t, _ := template.New("c").Parse(c)
+	content := new(bytes.Buffer)
+	t.Execute(content, m)
+	controller := content.String()
 	files.SaveFile(dir+"/app/"+name+"_controller.go", controller)
 	MakeView(name, dir+"/views/"+name+"_index.html")
 
-	fmt.Printf("\nr.Paths[\"%s\"] = app.Handle%s\n", name, camel)
+	fmt.Printf("\nr.Paths[\"%s\"] = app.Handle%s\n", name, m["camel"])
 }
 
 func MakeView(name, path string) {
