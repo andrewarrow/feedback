3870f1ade6a5e85d207e59dac0256dc259d99709
Andrew Arrow <oneone@gmail.com>
2023-04-06 11:12:15 -0700

    adding where in logic

---
+++ router/context_where_in.go
@@ -0,0 +1,34 @@
+package router
+
+import (
+	"fmt"
+	"strings"
+)
+
+func (c *Context) WhereInMap(modelString string, rows any, field, otherField string) (map[int64]map[string]any, []string) {
+	resultMap := map[int64]map[string]any{}
+	list := []string{}
+	ids := []string{}
+
+	if field != "" {
+		for _, row := range rows.([]map[string]any) {
+			ids = append(ids, fmt.Sprintf("%d", row[field]))
+		}
+	} else {
+		for _, s := range rows.([]string) {
+			ids = append(ids, s)
+		}
+	}
+
+	sql := fmt.Sprintf("where id in (%s)", strings.Join(ids, ","))
+	items := c.SelectAll(modelString, sql, []any{}, "")
+	for _, row := range items {
+		id := row["id"].(int64)
+		resultMap[id] = row
+		if otherField != "" {
+			list = append(list, fmt.Sprintf("%d", row[otherField]))
+		}
+	}
+
+	return resultMap, list
+}
