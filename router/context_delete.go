5fa316b9af26a239da3c38ade9fbff81364e6285
Andrew Arrow <oneone@gmail.com>
2023-04-10 16:04:40 -0700

    fixing delete and array json

--- router/context_delete.go
+++ router/context_delete.go
@@ -2,7 +2,7 @@ package router
 
 import "fmt"
 
-func (c *Context) Delete(modelName, fieldName string, id int64) {
+func (c *Context) Delete(modelName, fieldName string, id any) {
 	model := c.FindModel(modelName)
 	sql := fmt.Sprintf("delete from %s where %s=$1", model.TableName(), fieldName)
 	c.Db.Exec(sql, id)
--- router/context_params.go
+++ router/context_params.go
@@ -10,6 +10,13 @@ func (c *Context) ReadJsonBodyIntoParams() {
 	json.Unmarshal([]byte(body), &c.Params)
 }
 
+func (c *Context) ReadJsonBodyAsArray() []any {
+	var list []any
+	body := c.BodyAsString()
+	json.Unmarshal([]byte(body), &list)
+	return list
+}
+
 func (c *Context) ExecuteTemplate(filename string, vars any) {
 	c.router.Template.ExecuteTemplate(c.Writer, filename, vars)
 }
