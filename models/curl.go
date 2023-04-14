ae041c541ee34249cabddb34675ee06576e1c48d
Andrew Arrow <oneone@gmail.com>
2023-04-14 11:25:46 -0700

    fixing quotes

--- models/curl.go
+++ models/curl.go
@@ -3,6 +3,9 @@ package models
 import (
 	"fmt"
 	"strings"
+	"time"
+
+	"github.com/andrewarrow/feedback/util"
 )
 
 func (m *Model) CurlCreatePayload() string {
@@ -17,8 +20,14 @@ func (m *Model) CurlCreatePayload() string {
 			tokens := strings.Split(name, "_")
 			name = tokens[0] + "_guid"
 		}
-		val := "hi"
-		buffer = append(buffer, fmt.Sprintf("%s%s%s: %s%s%s", q, name, q, q, val, q))
+		val := q + "some_string" + q
+		if strings.HasSuffix(name, "_guid") {
+			val = q + util.PseudoUuid() + q
+		} else if field.Flavor == "timestamp" {
+			val = fmt.Sprintf("%d", time.Now().Unix())
+		}
+
+		buffer = append(buffer, fmt.Sprintf("%s%s%s: %s", q, name, q, val))
 	}
 	list := strings.Join(buffer, ",")
 	return "'{" + list + "}'"
