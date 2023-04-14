a692553f3fd3aad131b0cfdc74d10243a6aec960
Andrew Arrow <oneone@gmail.com>
2023-04-14 07:53:35 -0700

    making funcs at end

--- models/routes.go
+++ models/routes.go
@@ -27,6 +27,10 @@ func (r *Route) Generate(root string) string {
 		logic := handlePath(root, path.Verb, path.Second, path.Third)
 		buffer = append(buffer, logic)
 	}
+	for _, path := range r.Paths {
+		logic := handleFuncs(path.Verb)
+		buffer = append(buffer, logic)
+	}
 
 	return strings.Join(buffer, "\n")
 }
@@ -75,6 +79,24 @@ func handlePath(root, verb, second, third string) string {
 		util.ToCamelCase(flavor))
 	m := map[string]string{"name": name, "params": params,
 		"second_eq": second_eq, "third_eq": third_eq, "method": verb}
+	t, _ := template.New("c").Parse(c)
+	content := new(bytes.Buffer)
+	t.Execute(content, m)
+	logic := content.String()
+
+	return logic
+
+}
+
+func handleFuncs(verb string) string {
+	c := `func {{ index . "name" }}(c *router.Context{{ index . "params" }}) {
+  c.ReadJsonBodyIntoParams()
+  //c.SendContentAsJsonMessage("no guid", 422)
+  c.SendRowAsJson("", nil)
+}`
+
+	params := ""
+	m := map[string]string{"name": "foo", "params": params}
 	t, _ := template.New("c").Parse(c)
 	content := new(bytes.Buffer)
 	t.Execute(content, m)
