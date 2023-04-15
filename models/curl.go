5482e356f1712712b17b5bc481047f202a383064
Andrew Arrow <oneone@gmail.com>
2023-04-14 17:55:21 -0700

    drying up val

--- models/curl.go
+++ models/curl.go
@@ -2,7 +2,6 @@ package models
 
 import (
 	"encoding/json"
-	"fmt"
 	"strings"
 	"time"
 
@@ -10,8 +9,7 @@ import (
 )
 
 func (m *Model) CurlPutPayload() string {
-	buffer := []string{}
-	q := `"`
+	payload := map[string]any{}
 	for _, field := range m.Fields {
 		if field.CommonExclude() {
 			continue
@@ -23,22 +21,16 @@ func (m *Model) CurlPutPayload() string {
 		if strings.HasSuffix(name, "_id") {
 			continue
 		}
-		val := q + "some_string" + q
-		if field.Flavor == "timestamp" {
-			val = fmt.Sprintf("%d", time.Now().Unix())
-		} else if field.Flavor == "int" {
-			val = "0"
-		}
-
-		buffer = append(buffer, fmt.Sprintf("%s%s%s: %s", q, name, q, val))
+		payload[name] = exampleVal(name, field.Flavor)
 	}
-	list := strings.Join(buffer, ",")
-	return "'{" + list + "}'"
+	asBytes, _ := json.Marshal(payload)
+	jsonString := string(asBytes)
+	tokens := strings.Split(jsonString, ",")
+	return "'" + strings.Join(tokens, ",\n") + "'"
 }
 
 func (m *Model) CurlPostPayload() string {
-	buffer := []string{}
-	q := `"`
+	payload := map[string]any{}
 	for _, field := range m.Fields {
 		if field.Required == "" {
 			continue
@@ -48,43 +40,17 @@ func (m *Model) CurlPostPayload() string {
 			tokens := strings.Split(name, "_")
 			name = tokens[0] + "_guid"
 		}
-		val := q + "some_string" + q
-		if strings.HasSuffix(name, "_guid") {
-			val = q + util.PseudoUuid() + q
-		} else if field.Flavor == "timestamp" {
-			val = fmt.Sprintf("%d", time.Now().Unix())
-		} else if field.Flavor == "int" {
-			val = "0"
-		}
-
-		buffer = append(buffer, fmt.Sprintf("%s%s%s: %s", q, name, q, val))
+		payload[name] = exampleVal(name, field.Flavor)
 	}
-	list := strings.Join(buffer, ",\n")
-	return "'{" + list + "}'"
+	asBytes, _ := json.Marshal(payload)
+	jsonString := string(asBytes)
+	tokens := strings.Split(jsonString, ",")
+	return "'" + strings.Join(tokens, ",\n") + "'"
 }
 
 func (m *Model) CurlResponse() string {
 	wrapper := map[string]any{}
-	payload := map[string]any{}
-
-	for _, field := range m.Fields {
-		name := util.ToSnakeCase(field.Name)
-		if strings.HasSuffix(name, "_id") {
-			tokens := strings.Split(name, "_")
-			name = tokens[0] + "_guid"
-		}
-		var val any
-		if strings.HasSuffix(name, "_guid") || name == "guid" {
-			val = util.PseudoUuid()
-		} else if field.Flavor == "timestamp" {
-			val = time.Now().Unix()
-		} else if field.Flavor == "int" {
-			val = 0
-		} else {
-			val = "some_string"
-		}
-		payload[name] = val
-	}
+	payload := m.ExampleAlFields()
 
 	modelName := util.ToSnakeCase(m.Name)
 	util.RemoveSensitiveKeys(payload)
@@ -92,4 +58,31 @@ func (m *Model) CurlResponse() string {
 
 	asBytes, _ := json.Marshal(wrapper)
 	return util.PipeToJq(string(asBytes))
+}
+
+func (m *Model) ExampleAlFields() map[string]any {
+	payload := map[string]any{}
+	for _, field := range m.Fields {
+		name := util.ToSnakeCase(field.Name)
+		if strings.HasSuffix(name, "_id") {
+			tokens := strings.Split(name, "_")
+			name = tokens[0] + "_guid"
+		}
+		payload[name] = exampleVal(name, field.Flavor)
+	}
+	return payload
+}
+
+func exampleVal(name, flavor string) any {
+	var val any
+	if strings.HasSuffix(name, "_guid") || name == "guid" {
+		val = util.PseudoUuid()
+	} else if flavor == "timestamp" {
+		val = time.Now().Unix()
+	} else if flavor == "int" {
+		val = 0
+	} else {
+		val = "some_string"
+	}
+	return val
 }
