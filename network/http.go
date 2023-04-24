57cc01904fb7866cc4a83033d7c280aef1252fa9
Andrew Arrow <oneone@gmail.com>
2023-04-24 10:42:31 -0700

    adding multipart post

--- network/http.go
+++ network/http.go
@@ -4,28 +4,29 @@ import (
 	"bytes"
 	"fmt"
 	"io"
+	"mime/multipart"
 	"net/http"
 	"os"
 	"time"
+
+	"github.com/andrewarrow/feedback/router"
 )
 
-func BaseUrl() string {
-	return "https://api.openai.com"
-}
+var BaseUrl = "https://api.openai.com"
 
 func DoGet(route string) string {
 	agent := "agent"
 
-	urlString := fmt.Sprintf("%s%s", BaseUrl(), route)
+	urlString := fmt.Sprintf("%s%s", BaseUrl, route)
 	request, _ := http.NewRequest("GET", urlString, nil)
 	request.Header.Set("User-Agent", agent)
 	request.Header.Set("Authorization", "Bearer "+os.Getenv("OPEN_AI"))
 	request.Header.Set("Content-Type", "application/json")
 	client := &http.Client{Timeout: time.Second * 5}
-	return DoHttpRead("GET", route, client, request)
+	return DoHttpRead(route, client, request)
 }
 
-func DoHttpRead(verb, route string, client *http.Client, request *http.Request) string {
+func DoHttpRead(route string, client *http.Client, request *http.Request) string {
 	resp, err := client.Do(request)
 	if err == nil {
 		defer resp.Body.Close()
@@ -51,12 +52,42 @@ func DoHttpRead(verb, route string, client *http.Client, request *http.Request) 
 
 func DoPost(route string, payload []byte) string {
 	body := bytes.NewBuffer(payload)
-	urlString := fmt.Sprintf("%s%s", BaseUrl(), route)
+	urlString := fmt.Sprintf("%s%s", BaseUrl, route)
 	request, _ := http.NewRequest("POST", urlString, body)
 	request.Header.Set("Content-Type", "application/json")
 	request.Header.Set("Authorization", "Bearer "+os.Getenv("OPEN_AI"))
 	request.Header.Set("Content-Type", "application/json")
 	client := &http.Client{Timeout: time.Second * 50}
 
-	return DoHttpRead("POST", route, client, request)
+	return DoHttpRead(route, client, request)
+}
+
+func DoMultiPartPost(c *router.Context, route, name string, payload []byte) string {
+
+	body := &bytes.Buffer{}
+	writer := multipart.NewWriter(body)
+
+	//writer.WriteField("name", "John Doe")
+
+	fileWriter, _ := writer.CreateFormFile(name, "multipart")
+	theData := bytes.NewBuffer(payload)
+	io.Copy(fileWriter, theData)
+
+	writer.Close()
+
+	urlString := fmt.Sprintf("%s%s", BaseUrl, route)
+	request, _ := http.NewRequest("POST", urlString, body)
+	request.Header.Set("Content-Type", writer.FormDataContentType())
+
+	userCookie := router.GetCookie(c, "user")
+	SetHeaders(userCookie, request)
+	client := &http.Client{Timeout: time.Second * 50}
+
+	return DoHttpRead(route, client, request)
+}
+
+func SetHeaders(bearer string, request *http.Request) {
+	request.Header.Set("Authorization", fmt.Sprintf("Bearer %s", bearer))
+	request.Header.Set("Content-Type", "application/json")
+	request.Header.Set("Accept", "application/json")
 }
