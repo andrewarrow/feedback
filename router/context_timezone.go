cf14ea04267e56eb9eb4a8851d2b40dcf5b265c7
Andrew Arrow <oneone@gmail.com>
2023-04-25 07:41:40 -0700

    fix timezones list

---
+++ router/context_timezone.go
@@ -0,0 +1,20 @@
+package router
+
+import "time"
+
+func (c *Context) TimezoneList(list []map[string]any,
+	field1, field2 string, tz *time.Location) {
+	for _, thing := range list {
+		t1 := thing[field1].(int64)
+		t2 := thing[field2].(int64)
+
+		newT1 := time.Unix(t1, 0).In(tz)
+		newT2 := time.Unix(t2, 0).In(tz)
+
+		thing[field1] = newT1.Unix()
+		thing[field2] = newT2.Unix()
+
+		thing[field1+"_human"] = newT1.Format(HUMAN)
+		thing[field2+"_human"] = newT2.Format(HUMAN)
+	}
+}
