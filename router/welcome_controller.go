a85b10422db532a825174d1d8ff4ce4fe4365da3
Andrew Arrow <oneone@gmail.com>
2023-04-27 12:29:13 -0700

    adding example table

--- router/welcome_controller.go
+++ router/welcome_controller.go
@@ -11,5 +11,18 @@ func handleWelcome(c *Context, second, third string) {
 }
 
 func handleWelcomeIndex(c *Context) {
-	c.SendContentInLayout("welcome_index.html", nil, 200)
+	list := []any{""}
+	params := map[string]any{}
+	params["thing"] = "hi"
+
+	colAttributes := map[int]string{}
+	colAttributes[1] = "w-1/2"
+
+	m := map[string]any{}
+	headers := []string{"title", "description", "price", "something", "else", ""}
+	m["headers"] = headers
+	m["cells"] = c.MakeCells(list, headers, params, "_welcome_show")
+	m["col_attributes"] = colAttributes
+
+	c.SendContentInLayout("table_show.html", m, 200)
 }
