076f1ddf74d74c33cc374badfc42d4980b3a23d4
Andrew Arrow <oneone@gmail.com>
2023-04-29 12:00:52 -0700

    using BatchWriter

--- router/functions_to_run.go
+++ router/functions_to_run.go
@@ -2,6 +2,7 @@ package router
 
 import (
 	"fmt"
+	"net/http"
 	"strings"
 )
 
@@ -16,6 +17,11 @@ type Batch struct {
 func (c *Context) FunctionToRun(route string) *Batch {
 	b := Batch{}
 	b.Context = &Context{}
+	b.Context.Db = c.Db
+	b.Context.Writer = &BatchWriter{}
+
+	request, _ := http.NewRequest("GET", "/", nil)
+	b.Context.Request = request
 
 	tokens := strings.Split(route, "?")
 	noParams := tokens[0]
@@ -23,7 +29,7 @@ func (c *Context) FunctionToRun(route string) *Batch {
 		b.Params = tokens[1]
 	}
 	fmt.Println("noParams", noParams)
-	b.Context.tokens = strings.Split(noParams, "/")
+	b.Context.tokens = strings.Split(noParams+"/", "/")
 	first := b.Context.tokens[1]
 
 	second := ""
@@ -33,10 +39,28 @@ func (c *Context) FunctionToRun(route string) *Batch {
 	} else if len(b.Context.tokens) == 5 {
 		second = b.Context.tokens[2]
 		third = b.Context.tokens[3]
-
 	}
 	b.Second = second
 	b.Third = third
 	b.TheFunc = c.router.pathFuncToRun(first)
 	return &b
+}
+
+type BatchWriter struct {
+	http.ResponseWriter
+}
+
+func (w *BatchWriter) WriteHeader(statusCode int) {
+	w.ResponseWriter.WriteHeader(statusCode)
+}
+
+func (w *BatchWriter) Write(data []byte) (int, error) {
+	return w.ResponseWriter.Write(data)
+}
+
+func myHandler(w http.ResponseWriter, r *http.Request) {
+	BatchWriter := &BatchWriter{ResponseWriter: w}
+
+	BatchWriter.WriteHeader(http.StatusOK)
+	BatchWriter.Write([]byte("Hello, world!"))
 }
