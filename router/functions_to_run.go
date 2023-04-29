4068d4c8f76ea87a38a3c937e6455afb39186cff
Andrew Arrow <oneone@gmail.com>
2023-04-29 11:51:09 -0700

    return proper batch struct

--- router/functions_to_run.go
+++ router/functions_to_run.go
@@ -5,14 +5,38 @@ import (
 	"strings"
 )
 
-func (c *Context) FunctionToRun(route string) func(*Context, string, string) {
-	newContext := Context{}
+type Batch struct {
+	TheFunc func(*Context, string, string)
+	Context *Context
+	Second  string
+	Third   string
+	Params  string
+}
+
+func (c *Context) FunctionToRun(route string) *Batch {
+	b := Batch{}
+	b.Context = &Context{}
 
 	tokens := strings.Split(route, "?")
 	noParams := tokens[0]
+	if len(tokens) == 2 {
+		b.Params = tokens[1]
+	}
 	fmt.Println("noParams", noParams)
-	newContext.tokens = strings.Split(noParams, "/")
-	first := newContext.tokens[1]
+	b.Context.tokens = strings.Split(noParams, "/")
+	first := b.Context.tokens[1]
 
-	return c.router.pathFuncToRun(first)
+	second := ""
+	third := ""
+	if len(b.Context.tokens) == 4 {
+		second = b.Context.tokens[2]
+	} else if len(b.Context.tokens) == 5 {
+		second = b.Context.tokens[2]
+		third = b.Context.tokens[3]
+
+	}
+	b.Second = second
+	b.Third = third
+	b.TheFunc = c.router.pathFuncToRun(first)
+	return &b
 }
