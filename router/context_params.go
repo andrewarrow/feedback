bb31c6fcf2ab299b27b973ee5ceaab0548c6b88d
Andrew Arrow <oneone@gmail.com>
2023-05-05 09:08:05 -0700

    adding log file

--- router/context_params.go
+++ router/context_params.go
@@ -2,6 +2,9 @@ package router
 
 import (
 	"encoding/json"
+	"os"
+
+	"github.com/andrewarrow/feedback/files"
 )
 
 func (c *Context) ReadJsonBodyIntoParams() {
@@ -20,4 +23,17 @@ func (c *Context) ReadJsonBodyAsArray() []any {
 
 func (c *Context) ExecuteTemplate(filename string, vars any) {
 	c.router.Template.ExecuteTemplate(c.Writer, filename, vars)
+}
+
+func (c *Context) ReadJsonBodyIntoParamsWithLog(file string) {
+	home := files.UserHomeDir()
+	filename := home + "/" + file
+	f, _ := os.OpenFile(filename, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0644)
+
+	c.Params = map[string]any{}
+	body := c.BodyAsString()
+
+	defer f.Close()
+	f.WriteString(body)
+	json.Unmarshal([]byte(body), &c.Params)
 }
