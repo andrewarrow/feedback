1ba640abfc27fdbe9d02f211f13564d9edab9fb2
Andrew Arrow <oneone@gmail.com>
2023-05-05 19:07:12 -0700

    adding upsert

---
+++ router/upsert.go
@@ -0,0 +1,14 @@
+package router
+
+import "github.com/andrewarrow/feedback/sqlgen"
+
+func (c *Context) Upsert(modelString, where string, lastParam any) string {
+	model := c.FindModel(modelString)
+	tableName := model.TableName()
+	sql, params := sqlgen.InsertRowNoRandomDefaults(tableName, model.Fields, c.Params)
+	_, err := c.Db.Exec(sql, params...)
+	if err != nil {
+		return c.Update(modelString, where, lastParam)
+	}
+	return ""
+}
