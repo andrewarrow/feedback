bc2068d838cb89e05a1b3217e06ccd3842852a38
Andrew Arrow <oneone@gmail.com>
2023-05-27 07:54:32 -0700

    where in addition

--- router/context_where_in.go
+++ router/context_where_in.go
@@ -36,3 +36,39 @@ func (c *Context) WhereInWithId(modelString, id string, ids []any) map[int64]map
 
 	return resultMap
 }
+
+/*
+
+select coach_id, tag from coach_tags where tag in ('test2','test4') order by created_at desc limit 30;
+*/
+
+func listSizeOf(size int) string {
+	buffer := []string{}
+	for i := 1; i < size+1; i++ {
+		buffer = append(buffer, fmt.Sprintf("$%d", i))
+	}
+
+	return strings.Join(buffer, ",")
+}
+
+func (c *Context) AllIn(fields, modelName string, offset, other string, tokens []any) []map[string]any {
+	model := c.FindModel(modelName)
+	offsetString := ""
+	if offset != "" {
+		offsetString = "OFFSET " + offset
+	}
+	sql := fmt.Sprintf("select %s from %s where %s in (%s) order by created_at desc limit 30 %s", fields, model.TableName(), other, listSizeOf(len(tokens)), offsetString)
+	ms := []map[string]any{}
+	rows, err := c.Db.Queryx(sql, tokens...)
+	if err != nil {
+		return ms
+	}
+	defer rows.Close()
+	for rows.Next() {
+		m := make(map[string]any)
+		rows.MapScan(m)
+		CastFields(model, m)
+		ms = append(ms, m)
+	}
+	return ms
+}
