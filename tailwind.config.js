cce97be2f256e0dfb19c6423012306a28dabc072
Andrew Arrow <oneone@gmail.com>
2023-07-18 07:37:33 -0700

    tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['views/*.html','tailwind/*.html',],
+  theme: {
+    extend: {
+      colors: {
+        'cream': '#EFDECD',
+        'lime': '#8FBC8F'
+      },
+    },
+  },
+  plugins: [],
+}
