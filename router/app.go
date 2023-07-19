96b00e205dec4c14e7e71149aea1a32b0757234e
Andrew Arrow <oneone@gmail.com>
2023-07-19 12:01:20 -0700

    list of files

--- router/app.go
+++ router/app.go
@@ -15,7 +15,20 @@ func InitNewApp(path string) {
 		os.MkdirAll(place, 0755)
 		list, _ := ioutil.ReadDir(dir)
 		for _, file := range list {
-			path := dir + "/" + file.Name()
+			name := file.Name()
+			if dir == "views" {
+				if name != "_table_large.html" && name != "_table_small.html" &&
+					name != "_welcome_show_cols.html" &&
+					name != "_nav_user.html" &&
+					name != "application_layout.html" &&
+					name != "sessions_new.html" &&
+					name != "table_show.html" &&
+					name != "404.html" &&
+					name != "generic_top_bottom.html" {
+					continue
+				}
+			}
+			path := dir + "/" + name
 			asString := files.ReadFile(path)
 			files.SaveFile(place+"/"+file.Name(), asString)
 		}
