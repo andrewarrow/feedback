e6f1ea97ffb5400a8b16bd32a871b97709e1e57a
Andrew Arrow <oneone@gmail.com>
2023-07-22 11:26:06 -0700

    to any

--- util/array.go
+++ util/array.go
@@ -7,3 +7,11 @@ func ToAnyArray(rows []map[string]any) []any {
 	}
 	return list
 }
+
+func ToAny(rows []any) []any {
+	list := []any{}
+	for _, row := range rows {
+		list = append(list, row)
+	}
+	return list
+}
