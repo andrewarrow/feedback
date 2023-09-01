2d7fa959bb94a04405cd64ddf71b827c909ab8f7
Andrew Arrow <oneone@gmail.com>
2023-09-01 10:18:20 -0700

    location

--- location/zip.go
+++ location/zip.go
@@ -3,7 +3,9 @@ package location
 import (
 	"encoding/json"
 	"fmt"
+	"io"
 	"io/ioutil"
+	"os"
 	"strings"
 )
 
@@ -14,6 +16,52 @@ func ReadInZips(dirPath string) {
 		ReadInZipsState(filename)
 	}
 }
+
+func processLine(line string) {
+	line = strings.TrimSpace(line)
+	if line == "" {
+		fmt.Println("no zip")
+		return
+	}
+	var m map[string]any
+	json.Unmarshal([]byte(line), &m)
+	properties := m["properties"].(map[string]any)
+	zip, ok := properties["postcode"].(string)
+	if !ok {
+		fmt.Println("no zip", line)
+		return
+	}
+	geo := m["geometry"].(map[string]any)
+	latlong := geo["coordinates"].([]any)
+	if len(latlong) == 2 && len(zip) == 5 {
+		fmt.Println(zip, latlong)
+	} else {
+		fmt.Println("no zip", line)
+	}
+}
+
+func handleFileInBatches(filename string) {
+	file, _ := os.Open(filename)
+	buffer := make([]byte, 1)
+	line := []string{}
+	for {
+		n, err := file.Read(buffer)
+
+		if err == io.EOF || n == 0 {
+			break
+		}
+
+		s := string(buffer)
+		if s == "\n" {
+			theLine := strings.Join(line, "")
+			processLine(theLine)
+			line = []string{}
+		}
+		line = append(line, s)
+	}
+	file.Close()
+}
+
 func ReadInZipsState(dirPath string) {
 	// from https://openaddresses.io/
 	// https://batch.openaddresses.io/data
@@ -25,28 +73,6 @@ func ReadInZipsState(dirPath string) {
 		if strings.HasSuffix(filename, ".meta") {
 			continue
 		}
-		//fmt.Println(filename)
-		b, _ := ioutil.ReadFile(filename)
-		asString := string(b)
-		//fmt.Println(len(b))
-		lines := strings.Split(asString, "\n")
-		for _, line := range lines {
-			line = strings.TrimSpace(line)
-			if line == "" {
-				continue
-			}
-			var m map[string]any
-			json.Unmarshal([]byte(line), &m)
-			properties := m["properties"].(map[string]any)
-			zip, ok := properties["postcode"].(string)
-			if !ok {
-				continue
-			}
-			geo := m["geometry"].(map[string]any)
-			latlong := geo["coordinates"].([]any)
-			if len(latlong) == 2 && len(zip) == 5 {
-				fmt.Println(zip, latlong)
-			}
-		}
+		handleFileInBatches(filename)
 	}
 }
