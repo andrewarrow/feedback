1b75655befa0808f3c082b0b18b7d994274a415b
Andrew Arrow <oneone@gmail.com>
2023-09-01 09:46:46 -0700

    location

--- location/zip.go
+++ location/zip.go
@@ -8,6 +8,15 @@ import (
 )
 
 func ReadInZips(dirPath string) {
+	files, _ := ioutil.ReadDir(dirPath)
+	for _, file := range files {
+		filename := dirPath + "/" + file.Name()
+		ReadInZipsState(filename)
+	}
+}
+func ReadInZipsState(dirPath string) {
+	// from https://openaddresses.io/
+	// https://batch.openaddresses.io/data
 	// alameda-addresses-county.geojson
 	// {"type":"Feature","properties":{"hash":"9967fbc6d2dd1931","number":"41829","street":"OSGOOD RD","unit":"","city":"FREMONT","district":"","region":"","postcode":"94539","id":"525 034200500"},"geometry":{"type":"Point","coordinates":[-121.952505,37.5293622]}}
 	files, _ := ioutil.ReadDir(dirPath)
