673031ac7e57ee43336f2900364b48bf1a996e6e
Andrew Arrow <oneone@gmail.com>
2023-09-06 04:59:46 -0700

    mutliple

--- router/forms.go
+++ router/forms.go
@@ -5,7 +5,13 @@ import "strings"
 func (c *Context) ReadFormValuesIntoParams(list ...string) {
 	c.Params = map[string]any{}
 	for _, name := range list {
-		val := strings.TrimSpace(c.Request.FormValue(name))
+		selectedValues := c.Request.PostForm[name]
+		buffer := []string{}
+		for _, item := range selectedValues {
+			buffer = append(buffer, strings.TrimSpace(item))
+		}
+
+		val := strings.Join(buffer, ",")
 		c.Params[name] = val
 	}
 }
