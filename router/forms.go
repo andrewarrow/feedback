0587cd69907dd94576df1c5cfb4cecd5ed02d50f
Andrew Arrow <oneone@gmail.com>
2023-09-07 11:14:02 -0700

    form

--- router/forms.go
+++ router/forms.go
@@ -1,6 +1,12 @@
 package router
 
-import "strings"
+import (
+	"io"
+	"io/ioutil"
+	"strings"
+
+	"github.com/andrewarrow/feedback/util"
+)
 
 func (c *Context) ReadFormValuesIntoParams(list ...string) {
 	c.Params = map[string]any{}
@@ -21,5 +27,19 @@ func (c *Context) ReadMultipleFormValues(list ...string) {
 
 		val := strings.Join(buffer, ",")
 		c.Params[name] = val
+	}
+}
+
+func SaveMultiFiles(c *Context) {
+	files := c.Request.MultipartForm.File["file"]
+
+	for _, fileHeader := range files {
+		name := fileHeader.Filename
+		file, _ := fileHeader.Open()
+		asBytes, _ := io.ReadAll(file)
+		file.Close()
+		filename := util.GuidFilename(name)
+		ioutil.WriteFile(c.Router.BucketPath+"/"+filename, asBytes, 0644)
+		c.Params["photo"] = filename
 	}
 }
