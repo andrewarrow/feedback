f8f8290d9847096195048d8c0bf75beb7713dc4c
Andrew Arrow <oneone@gmail.com>
2023-09-14 07:34:43 -0700

    table

--- tool/table.go
+++ tool/table.go
@@ -9,6 +9,21 @@ import (
 	"github.com/andrewarrow/feedback/router"
 )
 
+func list(path, name string) {
+	asBytes, _ := ioutil.ReadFile(path + "/app/feedback.json")
+	var site router.FeedbackSite
+	json.Unmarshal(asBytes, &site)
+	//m := site.FindModel(name)
+
+	fmt.Println("func handleFoo(c *router.Context) {")
+	fmt.Printf(`list := c.All("%s", "order by created_at desc", "")\n`, name)
+	send := `send := map[string]any{}
+	send["list"] = list
+	c.SendContentInLayout("foo.html", send, 200)`
+	fmt.Println(send)
+	fmt.Println("}")
+}
+
 func table(path, name string) {
 	asBytes, _ := ioutil.ReadFile(path + "/app/feedback.json")
 	var site router.FeedbackSite
