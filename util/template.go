3958d5fbde0cdbf30d9eafbe1e3d01b5ec33ff05
Andrew Arrow <oneone@gmail.com>
2023-09-23 14:15:10 -0700

    template

---
+++ util/template.go
@@ -0,0 +1,16 @@
+package util
+
+import (
+	"fmt"
+	"io/ioutil"
+)
+
+func makeTemplate(name string) {
+	asBytes, _ := ioutil.ReadFile("t")
+	top := `{{ define "_%s" }}`
+	end := `{{ end }}`
+	s := fmt.Sprintf(top, name) + "\n" + string(asBytes) + "\n" + end
+	ioutil.WriteFile("views/_"+name+".html", []byte(s), 0644)
+	result := fmt.Sprintf(`{{ template "_%s" . }}`, name)
+	fmt.Printf("\n\n%s\n\n", result)
+}
