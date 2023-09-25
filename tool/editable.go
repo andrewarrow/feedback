6cab1590baa2d8c8b722b3f94232a933523bc4f7
Andrew Arrow <oneone@gmail.com>
2023-09-25 13:31:44 -0700

    flavor

--- tool/editable.go
+++ tool/editable.go
@@ -19,14 +19,8 @@ func editable(path, name string) {
 	for _, field := range m.Fields {
 		fmt.Println("<tr>")
 		fmt.Println(`{{$value := index $row "` + field.Name + `"}}`)
-		t := `
-<td class="px-3 py-2">
-  %s<br/>
-  {{ textfield "%s" $value}}
-</td>
-`
 		if field.Flavor == "photo" {
-			t = `
+			t := `
 <td class="px-3 py-2">
   %s<br/>
     {{ if eq $value ""}}
@@ -38,8 +32,16 @@ func editable(path, name string) {
    <input type="file" name="file" accept="image/jpeg, image/png, image/gif"/>
 </td>
 `
+			fmt.Printf(t, field.Name)
+		} else {
+			t := `
+<td class="px-3 py-2">
+  %s<br/>
+  {{ textfield "%s" $value}}
+</td>
+`
+			fmt.Printf(t, field.Name, field.Name)
 		}
-		fmt.Printf(t, field.Name, field.Name)
 		fmt.Println("</tr>")
 	}
 	fmt.Println("</table>")
