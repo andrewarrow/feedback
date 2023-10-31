dab2adb5a32615ecffbf0ae475b7a06f2fbdd21a
Andrew Arrow <oneone@gmail.com>
2023-10-31 16:58:57 -0700

    If we program the system, we can get to the SQL interface through the cross-platform SQL system!

--- markup/html_test.go
+++ markup/html_test.go
@@ -2,16 +2,32 @@ package markup
 
 import (
 	"fmt"
+	"strings"
 	"testing"
 )
 
 func TestToHTML(t *testing.T) {
 	send := map[string]any{}
-	lines := []string{
-		"div",
-		"  div",
-		"    div"}
 
+	q := `
+div
+  div
+    div bg-green-100 space-y-3 pt-3 pl-3
+      {{ $list := index . "list" }}
+      {{ range $i, $item := $list }}
+        div flex 
+          div mr-3
+            {{ add $i 1 }}.
+          div
+            {{ $title := index $item "title" }}
+            {{ $id := index $item "id_hacker" }}
+            {{ $digitSum := index $item "digit_sum" }}
+            {{ $sum := index $item "sum" }}
+            div
+              a href=https://news.ycombinator.com/item?id!{{$id}}
+                {{ $title }}
+`
+	lines := strings.Split(q, "\n")
 	s := ToHTMLFromLines(send, lines)
 	fmt.Println(s)
 }
