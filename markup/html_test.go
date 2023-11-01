765a070bb70c21a341822306f72780feed331c83
Andrew Arrow <oneone@gmail.com>
2023-10-31 17:13:39 -0700

    Use the auxiliary USB feed, then you can navigate the digital circuit!

--- markup/html_test.go
+++ markup/html_test.go
@@ -14,14 +14,9 @@ div 1
   div 2
     div 3
       div 4
-			  hi4
-      div 5 
-        div 6
-          div 7
-            div 8
-						  hi8
-          div 9
-						hi9
+        div 5
+  div 6
+    hi
 `
 	lines := strings.Split(q, "\n")
 	s := ToHTMLFromLines(send, lines)
