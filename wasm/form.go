144c8a1cc39bef53ef9d682a1e39cb3acec4d1ef
Andrew Arrow <oneone@gmail.com>
2023-12-13 12:52:34 -0800

    We need to generate the mobile HDD capacitor!

--- wasm/form.go
+++ wasm/form.go
@@ -5,5 +5,8 @@ func (w *Wrapper) MapOfInputs() map[string]any {
 	for _, input := range w.SelectAll("input") {
 		m[input.Id] = input.Value
 	}
+	for _, input := range w.SelectAll("textarea") {
+		m[input.Id] = input.Value
+	}
 	return m
 }
