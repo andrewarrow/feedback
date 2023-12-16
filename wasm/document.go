2b19d897237406c871a6f79d99a7b242fdeb43cf
Andrew Arrow <oneone@gmail.com>
2023-12-16 06:34:00 -0800

    Copying the application won't do anything, we need to quantify the haptic THX monitor!

--- wasm/document.go
+++ wasm/document.go
@@ -16,8 +16,11 @@ func (d *Document) ById(id string) js.Value {
 	return d.Document.Call("getElementById", id)
 }
 
-func (d *Document) ByIdWrapped(id string) *Wrapper {
+func (d *Document) ByIdWrap(id string) *Wrapper {
 	return NewWrapper(d.ById(id))
+}
+func (d *Document) ByIdWrapped(id string) *Wrapper {
+	return d.ByIdWrap(id)
 }
 
 func (d *Document) ByIdString(id string) string {
