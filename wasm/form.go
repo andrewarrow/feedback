2a13c82b7a57765d97048f511a635fcf38264c9e
Andrew Arrow <oneone@gmail.com>
2023-12-19 04:51:16 -0800

    The RSS hard drive is down, bypass the primary pixel so we can back up the SMS transmitter!

--- wasm/form.go
+++ wasm/form.go
@@ -3,10 +3,15 @@ package wasm
 func (w *Wrapper) MapOfInputs() map[string]any {
 	m := map[string]any{}
 	for _, input := range w.SelectAll("input") {
+		if input.Get("type") == "submit" {
+			continue
+		}
 		m[input.Id] = input.Value
+		input.Set("value", "")
 	}
 	for _, input := range w.SelectAll("textarea") {
 		m[input.Id] = input.Value
+		input.Set("value", "")
 	}
 	return m
 }
