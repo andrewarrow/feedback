a288b60ffe3b8cc8e341542750f0ccfd875834b9
Andrew Arrow <oneone@gmail.com>
2023-12-20 08:56:57 -0800

    You can't synthesize the port without connecting the open-source USB circuit!

--- router/viewport.go
+++ router/viewport.go
@@ -2,4 +2,9 @@ package router
 
 import "html/template"
 
-var viewport = template.HTML(`<meta name="viewport" content="width=device-width, initial-scale=1.0">`)
+var viewport = template.HTML(
+	`<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link href="https://fonts.bunny.net" rel="preconnect"/>
+<link href="https://fonts.bunny.net/css?family=poppins:400,500,700,800" rel="stylesheet"/>
+<link href="https://fonts.bunny.net/css?family=montserrat:400,500,700,800" rel="stylesheet"/>
+`)
