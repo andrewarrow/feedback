022d042433e2d0521baf5cbad77980bff1ccdac0
Andrew Arrow <oneone@gmail.com>
2024-01-04 12:25:49 -0800

    Use the primary CSS firewall, then you can transmit the redundant panel!

--- wasm/global.go
+++ wasm/global.go
@@ -49,3 +49,7 @@ func (g *Global) Submit(id string, fn func(js.Value, []js.Value) any) {
 	form := g.Document.ById(id)
 	form.Set("onsubmit", js.FuncOf(fn))
 }
+func (g *Global) Focus(id string, fn func(js.Value, []js.Value) any) {
+	form := g.Document.ById(id)
+	form.Set("onfocus", js.FuncOf(fn))
+}
