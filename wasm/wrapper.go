bb670b9758591d284b86d560f21c80db6d2ff48b
Andrew Arrow <oneone@gmail.com>
2024-02-21 21:49:12 -0800

    Fix bug in main path

--- wasm/wrapper.go
+++ wasm/wrapper.go
@@ -75,10 +75,10 @@ func (w *Wrapper) GetItem(key any) string {
 func (w *Wrapper) Click(fn func(js.Value, []js.Value) any) {
 	w.JValue.Set("onclick", js.FuncOf(fn))
 }
-func (w *Wrapper) Event(fn func()) {
+func (w *Wrapper) Event(fn func(id string)) {
 	thefunc := func(this js.Value, p []js.Value) any {
 		p[0].Call("preventDefault")
-		fn()
+		fn(p[0].Get("target").Get("id").String())
 		return nil
 	}
 	w.JValue.Set("onclick", js.FuncOf(thefunc))
