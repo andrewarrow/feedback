f8cabdb98c1fe85387a1e9420aa22fcfc7af468c
Andrew Arrow <oneone@gmail.com>
2024-02-25 15:08:52 -0800

    Implement code review feedback

--- sqlgen/geometry.go
+++ sqlgen/geometry.go
@@ -1,23 +1,14 @@
 package sqlgen
 
 import (
-	"bytes"
-	"encoding/binary"
-	"encoding/hex"
+	"github.com/twpayne/go-geom"
+	"github.com/twpayne/go-geom/encoding/ewkbhex"
 )
 
 func CreatePointHexRepresentation(longitude, latitude float64) string {
-	lonHex, _ := float64ToHex(longitude)
-	latHex, _ := float64ToHex(latitude)
-	pointHex := lonHex + latHex
-	return pointHex
-}
 
-func float64ToHex(value float64) (string, error) {
-	buf := new(bytes.Buffer)
-	err := binary.Write(buf, binary.LittleEndian, value)
-	if err != nil {
-		return "", err
-	}
-	return hex.EncodeToString(buf.Bytes()), nil
+	point := geom.NewPoint(geom.XY).MustSetCoords([]float64{longitude, latitude}).SetSRID(4326)
+
+	hex, _ := ewkbhex.Encode(point, ewkbhex.NDR)
+	return hex
 }
