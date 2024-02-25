26fae58489cedbdd632951ac9a4c6838598f0808
Andrew Arrow <oneone@gmail.com>
2024-02-25 09:44:00 -0800

    Resolve merge conflict

---
+++ wasm/http_bearer.go
@@ -0,0 +1,36 @@
+package wasm
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"time"
+)
+
+func DoHttpBearerRead(bearer string, request *http.Request) (string, int) {
+	client := &http.Client{Timeout: time.Second * 5}
+	request.Header.Set("Content-Type", "application/json")
+	request.Header.Set("Authorization", "Bearer: "+bearer)
+	resp, err := client.Do(request)
+	if err == nil {
+		defer resp.Body.Close()
+		body, err := io.ReadAll(resp.Body)
+		if err != nil {
+			fmt.Printf("\n\nERROR: %d %s\n\n", resp.StatusCode, err.Error())
+			return err.Error(), 500
+		}
+		return string(body), resp.StatusCode
+	}
+	fmt.Printf("\n\nERROR: %s\n\n", err.Error())
+	return err.Error(), 500
+}
+
+func DoBearerGet(bearer, urlString string) string {
+	request, err := http.NewRequest("GET", urlString, nil)
+	if err != nil {
+		return ""
+	}
+
+	jsonString, _ := DoHttpBearerRead(bearer, request)
+	return jsonString
+}
