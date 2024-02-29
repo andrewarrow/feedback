42c741767325f9546df5fd2a67161fea712581f7
Andrew Arrow <oneone@gmail.com>
2024-02-29 12:19:29 -0800

    Update version number

--- sqlgen/geometry.go
+++ sqlgen/geometry.go
@@ -2,6 +2,10 @@ package sqlgen
 
 import (
 	"fmt"
+	"strconv"
+	"strings"
+
+	//"github.com/twpayne/cockroach/geo"
 
 	"github.com/twpayne/go-geom"
 	"github.com/twpayne/go-geom/encoding/ewkbhex"
@@ -17,7 +21,21 @@ func CreatePointHexRepresentation(longitude, latitude float64) string {
 
 func HexToPoint(s string) (float64, float64) {
 	decoded, _ := ewkbhex.Decode(s)
-	fmt.Println(decoded)
+	txt := fmt.Sprintf("%v", decoded)
+	//&{{1 2 [34.123 -118.34] 4326}}
+	//34.123 -118.34] 4326}}
+	first := strings.Index(txt, "[")
+	prefix := txt[first+1:]
+	last := strings.Index(prefix, "]")
+	tokens := strings.Split(prefix[0:last], " ")
+	//fmt.Println(tokens[0], tokens[1])
+	latFloat, _ := strconv.ParseFloat(tokens[0], 64)
+	lonFloat, _ := strconv.ParseFloat(tokens[1], 64)
+	return latFloat, lonFloat
+
+	//ret, _ := ewkb.Marshal(decoded, binary.LittleEndian) //geo.DefaultEWKBEncodingFormat)
+	//thing := geopb.EWKB(ret)
+	//fmt.Printf("%+v\n", thing)
 	//point, ok := decoded.(*geom.Point)
 	//coords := point.Coords()
 	//return coords[0], coords[1]
