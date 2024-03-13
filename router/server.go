14c2ba33ca2fdb5918177d0d85327db5d27913ee
Andrew Arrow <oneone@gmail.com>
2024-03-12 19:29:02 -0700

    tls

--- router/server.go
+++ router/server.go
@@ -1,6 +1,8 @@
 package router
 
 import (
+	"crypto/tls"
+	"fmt"
 	"log"
 	"net/http"
 )
@@ -11,4 +13,22 @@ func (r *Router) ListenAndServe(port string) {
 	})
 
 	log.Fatal(http.ListenAndServe(port, nil))
+}
+
+func (r *Router) ListenAndServeTLS() {
+	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
+		r.RouteFromRequest(writer, request)
+	})
+
+	server := &http.Server{
+		Addr: ":443",
+		TLSConfig: &tls.Config{
+			InsecureSkipVerify: true,
+		},
+	}
+
+	err := server.ListenAndServeTLS("cert.pem", "key.pem")
+	fmt.Println(err)
+	select {}
+
 }
