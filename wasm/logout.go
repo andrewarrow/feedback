4c0ecca43de391527446336a7af5c04497d7649a
Andrew Arrow <oneone@gmail.com>
2024-05-17 09:41:54 -0700

    Update error handling

--- wasm/logout.go
+++ wasm/logout.go
@@ -1,12 +1,14 @@
 package wasm
 
-func (g *Global) Logout(path string) {
-	go func() {
-		code := DoDelete(path + "/logout")
-		if code == 200 {
-			g.Location.Set("href", "/")
-			return
-		}
-		g.flashThree("error")
-	}()
+func (g *Global) Logout(path string) func() {
+	return func() {
+		go func() {
+			code := DoDelete(path + "/logout")
+			if code == 200 {
+				g.Location.Set("href", "/")
+				return
+			}
+			g.flashThree("error")
+		}()
+	}
 }
