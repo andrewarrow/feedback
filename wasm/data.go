86e669efe6191a3d8db5c4471e767863f88ff342
Andrew Arrow <oneone@gmail.com>
2024-05-21 09:45:15 -0700

    Resolve merge conflict

--- wasm/data.go
+++ wasm/data.go
@@ -1,6 +1,16 @@
 package wasm
 
-func (g *Global) LoadData(route, id string) []any {
-	d := []any{1, 2, 3}
-	return d
+import (
+	"encoding/json"
+	"strings"
+)
+
+func (g *Global) LoadData(route, guid string) []any {
+	tokens := strings.Split(guid, "-")
+	id := tokens[len(tokens)-1]
+	jsonString := DoGet(route + "&guid=" + id)
+	var m map[string]any
+	json.Unmarshal([]byte(jsonString), &m)
+	items, _ := m["items"].([]any)
+	return items
 }
