81985e5c57d86eea380d3971937369c0b6767178
Andrew Arrow <oneone@gmail.com>
2024-06-07 02:29:55 -0700

    Update configuration files

--- wasm/func.go
+++ wasm/func.go
@@ -2,9 +2,10 @@ package wasm
 
 import "syscall/js"
 
-func FuncOf(fn func()) {
-	thefunc := func(this js.Value, p []js.Value) any {
-		fn()
+func FuncOf(fn func(js.Value)) any {
+	theFunc := func(this js.Value, p []js.Value) any {
+		fn(p[0])
 		return nil
 	}
+	return js.FuncOf(theFunc)
 }
