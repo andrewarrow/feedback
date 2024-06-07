c98016c9c104cdc320912b104bcf9212bba5fbff
Andrew Arrow <oneone@gmail.com>
2024-06-07 03:07:56 -0700

    Refactor code structure

--- persist/sqlite.go
+++ persist/sqlite.go
@@ -8,7 +8,7 @@ import (
 )
 
 func SqliteConnection() *sqlx.DB {
-	db, err := sqlx.Connect("sqlite3", "sqlite.db")
+	db, err := sqlx.Connect("sqlite3", UserHomeDir()+"/epoch_sqlite_560dc8c4-b18a-4517-a90c-b0f92d2ba5a5.db")
 	if err != nil {
 		fmt.Println(err)
 		return nil
