3439a04cf038c792b4d578d5f5a304a83a5a2f06
Andrew Arrow <oneone@gmail.com>
2024-06-07 03:08:04 -0700

    Optimize performance

--- persist/sqlite.go
+++ persist/sqlite.go
@@ -3,12 +3,13 @@ package persist
 import (
 	"fmt"
 
+	"github.com/andrewarrow/feedback/files"
 	"github.com/jmoiron/sqlx"
 	_ "github.com/mattn/go-sqlite3"
 )
 
 func SqliteConnection() *sqlx.DB {
-	db, err := sqlx.Connect("sqlite3", UserHomeDir()+"/epoch_sqlite_560dc8c4-b18a-4517-a90c-b0f92d2ba5a5.db")
+	db, err := sqlx.Connect("sqlite3", files.UserHomeDir()+"/epoch_sqlite_560dc8c4-b18a-4517-a90c-b0f92d2ba5a5.db")
 	if err != nil {
 		fmt.Println(err)
 		return nil
