67ba995d01fbdc276e3e32db5b559a76357f687b
Andrew Arrow <oneone@gmail.com>
2024-06-08 21:01:58 -0700

    Reformat code

---
+++ wasm/auto_click.go
@@ -0,0 +1,16 @@
+package wasm
+
+import "fmt"
+
+func (g *Global) AutoClick(prefix, suffix string, w *Wrapper, name string, cb func(string)) {
+	for _, item := range w.SelectAllByClass(name) {
+		id := item.Id[2:]
+		click := func() {
+			go func() {
+				DoPost(fmt.Sprintf("/%s/%s/%s", prefix, id, suffix), nil)
+				cb("")
+			}()
+		}
+		item.EventWithId(click)
+	}
+}
