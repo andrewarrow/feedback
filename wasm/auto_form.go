5f6606f8503246e990f86147f3266972d6fa6a9f
Andrew Arrow <oneone@gmail.com>
2024-06-20 20:41:29 -0700

    resolved memory corruption issue

--- wasm/auto_form.go
+++ wasm/auto_form.go
@@ -2,13 +2,14 @@ package wasm
 
 import (
 	"encoding/json"
+	"net/url"
 	"syscall/js"
 )
 
 type AutoForm struct {
 	ReturnPath string
 	Path       string
-	Before     func()
+	Before     func() string
 	After      func(string)
 	Id         string
 	Method     string
@@ -25,22 +26,29 @@ func (g *Global) AddAutoForm(a *AutoForm) {
 	form := g.Document.Id(a.Id)
 	thefunc := func(this js.Value, p []js.Value) any {
 		p[0].Call("preventDefault")
+		q := ""
 		if a.Before != nil {
-			a.Before()
+			q = a.Before()
 		}
-		go a.Post(g, form)
+		go a.Post(g, form, q)
 		return nil
 	}
 	form.JValue.Set("onsubmit", js.FuncOf(thefunc))
 }
 
-func (a *AutoForm) Post(g *Global, w *Wrapper) {
+func (a *AutoForm) Post(g *Global, w *Wrapper, q string) {
 	var jsonString string
 	var code int
 	if a.Method == "POST" {
 		jsonString, code = DoPost(a.Path, w.MapOfInputs())
 	} else if a.Method == "PATCH" {
 		jsonString, code = DoPatch(a.Path, w.MapOfInputs())
+	} else if a.Method == "GET" {
+		qValue := url.QueryEscape(q)
+		jsonString = DoGet(a.Path + "?q=" + qValue)
+		if jsonString == "" {
+			code = 200
+		}
 	}
 	var m map[string]any
 	json.Unmarshal([]byte(jsonString), &m)
