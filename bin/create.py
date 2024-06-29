226e5087411deed765bbf77031d957cbccfba74b
Andrew Arrow <oneone@gmail.com>
2024-06-29 13:29:49 -0700

    removed deprecated code

--- bin/create.py
+++ bin/create.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env /Users/aa/rick/foo/bin/python3
 
+import sys
+
 def replace_template_vars(template, replacements):
     for key, value in replacements.items():
         template = template.replace(f"{{{{{key}}}}}", value)
     return template
 
 def foo():
+    path = sys.argv[1]
+    name = sys.argv[2]
     template = """\
     Hello, {{name}}!
     Welcome to {{place}}.
@@ -19,7 +23,7 @@ def foo():
 
     result = replace_template_vars(template, replacements)
 
-    output_filename = "test.txt"
+    output_filename = path+"/"+name+"/"+"go.mod"
     with open(output_filename, 'w') as file:
         file.write(result)
 
