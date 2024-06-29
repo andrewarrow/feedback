f21cd2b0884bd5b92a9b2c999188bbe5bccec2ad
Andrew Arrow <oneone@gmail.com>
2024-06-29 14:08:33 -0700

    updated copyright information

---
+++ bin/gomain.py
@@ -0,0 +1,53 @@
+
+from placeit import placeit
+
+def gomain():
+    template = """\
+package main
+
+import "fmt"
+
+//go:embed app/feedback.json
+var embeddedFile []byte
+
+//go:embed views/*.html
+var embeddedTemplates embed.FS
+
+//go:embed assets/**/*.*
+var embeddedAssets embed.FS
+
+var buildTag string
+
+func main() {
+	rand.Seed(time.Now().UnixNano())
+	if len(os.Args) == 1 {
+		//PrintHelp()
+		return
+	}
+
+	arg := os.Args[1]
+
+	if arg == "import" {
+	} else if arg == "render" {
+		router.RenderMarkup()
+	} else if arg == "run" {
+		router.BuildTag = buildTag
+		router.EmbeddedTemplates = embeddedTemplates
+		router.EmbeddedAssets = embeddedAssets
+		r := router.NewRouter("DATABASE_URL", embeddedFile)
+		r.Paths["/"] = app.HandleWelcome
+		r.Paths["book"] = app.HandleBook
+		r.Paths["api"] = app.HandleApi
+		//r.Paths["login"] = app.Login
+		//r.Paths["register"] = app.Register
+		//r.Paths["admin"] = app.Admin
+		r.Paths["markup"] = app.Markup
+		r.BucketPath = "/Users/aa/bucket"
+		r.ListenAndServe(":" + os.Args[2])
+	} else if arg == "help" {
+	}
+}
+    """
+
+    placeit("main.go", {}, template)
+
