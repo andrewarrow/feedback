b169fa28485bd5054de2a243cfb3d7f1abe0c33e
Andrew Arrow <oneone@gmail.com>
2024-06-29 13:28:10 -0700

    refactored exception handling

--- bin/create.py
+++ bin/create.py
@@ -1,3 +1,26 @@
 #!/usr/bin/env /Users/aa/rick/foo/bin/python3
 
-print("wefw")
+def replace_template_vars(template, replacements):
+    for key, value in replacements.items():
+        template = template.replace(f"{{{{{key}}}}}", value)
+    return template
+
+def foo():
+    template = """\
+    Hello, {{name}}!
+    Welcome to {{place}}.
+    We hope you enjoy your stay.
+    """
+
+    replacements = {
+        "name": "Alice",
+        "place": "Wonderland"
+    }
+
+    result = replace_template_vars(template, replacements)
+
+    output_filename = "test.txt"
+    with open(output_filename, 'w') as file:
+        file.write(result)
+
+foo()
