9151f91887c238900326f9da8b5cd21125f1bc7f
Andrew Arrow <oneone@gmail.com>
2024-07-22 00:33:21 -0700

    refactored error messages

--- models/structure.go
+++ models/structure.go
@@ -9,7 +9,7 @@ import (
 
 const ISO8601 = "2006-01-02T15:04:05-07:00"
 const HUMAN = "Monday, January 2, 2006 3:04 PM"
-const HUMAN_DATE = "January 2, 2006"
+const HUMAN_DATE = "2006-01-02"
 
 type Model struct {
 	Name   string   `json:"name"`
