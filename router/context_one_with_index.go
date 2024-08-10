a6d495dbe1371330cf9687d5b7ebf6dc9e4a5adc
Andrew Arrow <oneone@gmail.com>
2024-08-10 08:25:02 -0700

    resolved caching issue

---
+++ router/context_one_with_index.go
@@ -0,0 +1,18 @@
+package router
+
+import "fmt"
+
+func (c *Context) OneWithIndex(index int64, modelName string, where string, params []any) map[string]any {
+	model := c.FindModel(modelName)
+	sql := fmt.Sprintf("SELECT * FROM %s %s", model.TableName(), where)
+	m := map[string]any{}
+	rows, err := c.Dbs[index].Queryx(sql, params...)
+	if err != nil {
+		return m
+	}
+	defer rows.Close()
+	rows.Next()
+	rows.MapScan(m)
+	CastFields(model, m)
+	return m
+}
