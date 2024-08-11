6eb7a3f441e05e9bf61798e1f8fa8aaca52fe8b0
Andrew Arrow <oneone@gmail.com>
2024-08-11 03:57:28 -0700

    improved error detection

--- openapi/endpoint.go
+++ openapi/endpoint.go
@@ -1,9 +1,22 @@
 package openapi
 
+import "strings"
+
 type Endpoint struct {
+	Method string
 }
+
+var verbs = []string{"GET", "POST", "DELETE", "PATCH", "PUT"}
 
 func NewEndpoint(comment, line string) Endpoint {
 	ep := Endpoint{}
+
+	for _, verb := range verbs {
+		if strings.Contains(line, verb) {
+			ep.Method = verb
+			break
+		}
+	}
+
 	return ep
 }
