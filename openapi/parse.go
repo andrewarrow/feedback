e1c33bb93c8777294bb0c91349c6123cb5d69f90
Andrew Arrow <oneone@gmail.com>
2024-08-11 04:51:05 -0700

    added support for new operating system

--- openapi/parse.go
+++ openapi/parse.go
@@ -22,12 +22,14 @@ func Parse(path, dir string) {
 		items = append(items, ep)
 	}
 
-	fmt.Println(items)
+	//fmt.Println(items)
 	entries, _ := os.ReadDir(dir)
 	for _, entry := range entries {
-		fmt.Println(entry.Name())
+		//fmt.Println(entry.Name())
 		lookForParams(dir + "/" + entry.Name())
 	}
+
+	MakeYaml(items)
 
 }
 
@@ -36,13 +38,19 @@ func lookForParams(path string) {
 	s := string(b)
 	lines := strings.Split(s, "\n")
 	start := false
+	lastFunc := ""
 	for _, line := range lines {
 		trimmed := strings.TrimSpace(line)
+		if strings.HasPrefix(trimmed, "func ") {
+			tokens := strings.Split(trimmed, " ")
+			tokens = strings.Split(tokens[1], "(")
+			lastFunc = tokens[0]
+		}
 		if strings.HasPrefix(trimmed, "// oa end") {
 			start = false
 		}
 		if start {
-			fmt.Println(trimmed)
+			fmt.Println(lastFunc, trimmed)
 		}
 		if strings.HasPrefix(trimmed, "// oa start") {
 			start = true
---
+++ openapi/yaml.go
@@ -0,0 +1,26 @@
+package openapi
+
+import (
+	"fmt"
+	"strings"
+)
+
+func MakeYaml(eps []Endpoint) {
+
+	buffer := []string{}
+
+	for _, ep := range eps {
+		buffer = append(buffer, ep.Path+":")
+	}
+
+	final := yaml + "\n" + strings.Join(buffer, "\n")
+
+	fmt.Println(final)
+}
+
+var yaml = `openapi: 3.1.0
+info:
+  title: Feedback API
+  description: Feedback API 
+  version: 1.0.0
+paths:`
