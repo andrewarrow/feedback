61cd7a3731a3512ddf774d2c3a6e1d25bafd425f
Andrew Arrow <oneone@gmail.com>
2024-08-11 04:07:50 -0700

    added support for new HTTP method

--- openapi/endpoint.go
+++ openapi/endpoint.go
@@ -1,6 +1,9 @@
 package openapi
 
-import "strings"
+import (
+	"fmt"
+	"strings"
+)
 
 type Endpoint struct {
 	Method  string
@@ -21,6 +24,22 @@ func NewEndpoint(comment, line string) Endpoint {
 
 	tokens := strings.Split(comment, " ")
 	ep.Returns = tokens[len(tokens)-1]
+
+	tokens = strings.Split(line, "&&")
+	tokens = tokens[0 : len(tokens)-1]
+
+	for _, item := range tokens {
+		if strings.Contains(item, "==") {
+			tokens := strings.Split(item, "==")
+			fmt.Println(tokens[1])
+		} else if strings.Contains(item, "!=") {
+			tokens := strings.Split(item, "!=")
+			fmt.Println(tokens[1])
+		}
+	}
+
+	// second == "user" && third == "charge-history" && foo
+	// second == "user" && third != "" && bar
 
 	return ep
 }
