d1b49a4f01fd14764b3a7492e48c28aa82d2ef92
Andrew Arrow <oneone@gmail.com>
2024-08-11 07:52:28 -0700

    refactored UI components

--- openapi/scan.go
+++ openapi/scan.go
@@ -1,9 +1,34 @@
 package openapi
 
+import (
+	"os"
+	"strings"
+)
+
 type OpenAPI struct {
+	Endpoints map[string][]Endpoint
 }
 
 func ScanDir(dir string) *OpenAPI {
 	oa := OpenAPI{}
+	entries, _ := os.ReadDir(dir)
+	for _, entry := range entries {
+		name := entry.Name()
+		if strings.HasSuffix(name, ".go") == false {
+			continue
+		}
+		b, _ := os.ReadFile(dir + "/" + name)
+		s := string(b)
+		lines := strings.Split(s, "\n")
+		for i, line := range lines {
+			trimmed := strings.TrimSpace(line)
+			if strings.HasPrefix(trimmed, "// oa ") == false {
+				continue
+			}
+			target := lines[i+1]
+			ep := NewEndpoint(trimmed, target)
+			oa.Endpoints[ep.Path] = append(oa.Endpoints[ep.Path], ep)
+		}
+	}
 	return &oa
 }
